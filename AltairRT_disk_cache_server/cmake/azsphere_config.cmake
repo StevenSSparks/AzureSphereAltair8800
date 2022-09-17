6e6d7179c6f7a1be99c0d3380564ed4d48638910
Steve Sparks <15949542+StevenSSparks@users.noreply.github.com>
2022-09-17 18:26:27 -0500

    Update

--- AltairRT_disk_cache_server/cmake/azsphere_config.cmake
+++ AltairRT_disk_cache_server/cmake/azsphere_config.cmake
@@ -1,2 +1,2 @@
-azsphere_configure_tools(TOOLS_REVISION "21.07")
-azsphere_configure_api(TARGET_API_SET "10")
+azsphere_configure_tools(TOOLS_REVISION "22.07")
+azsphere_configure_api(TARGET_API_SET "System.Object[]")
